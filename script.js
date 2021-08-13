247f5aaea2332be01aa1b0fea3cb99205d66caaa
MR PARKER'S SON <princeparker@PRINCES-MacBook-Air.local>
2021-08-13 14:04:42 +0000

    Mopty

---
+++ script.js
@@ -0,0 +1,23 @@
+'use strict';
+
+// prettier-ignore
+const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+
+const form = document.querySelector('.form');
+const containerWorkouts = document.querySelector('.workouts');
+const inputType = document.querySelector('.form__input--type');
+const inputDistance = document.querySelector('.form__input--distance');
+const inputDuration = document.querySelector('.form__input--duration');
+const inputCadence = document.querySelector('.form__input--cadence');
+const inputElevation = document.querySelector('.form__input--elevation');
+
+
+if(navigator.geolocation)
+navigator.geolocation.getCurrentPosition(function(position){
+const {latitude} = position.coords;
+const {longitude} = position.coords;
+console.log(`https://www.google.co.uk/maps/@${latitude},${longitude}`)
+
+},function(){
+alert('Could not get your location')
+})
